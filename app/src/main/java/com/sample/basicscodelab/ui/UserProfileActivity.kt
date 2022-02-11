30dfae0959a51b87aaf3b18bba3170649dae26a8
Chris Assigbe <cassigbe@google.com>
2022-02-11 10:46:21 -0600

    Add logout mechanism to UserProfileActivity.kt

--- app/src/main/java/com/sample/basicscodelab/ui/UserProfileActivity.kt
+++ app/src/main/java/com/sample/basicscodelab/ui/UserProfileActivity.kt
@@ -7,12 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +24,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import com.android.billingclient.api.BillingFlowParams
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.sample.basicscodelab.MainActivity
 import com.sample.basicscodelab.billing.AppBillingClient
 import com.sample.basicscodelab.repository.SubscriptionDataRepository
 import com.sample.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -32,6 +36,7 @@ import com.sample.basicscodelab.ui.theme.BasicsCodelabTheme
 class UserProfileActivity : AppCompatActivity() {
     private lateinit var appBillingClient: AppBillingClient
     private lateinit var repo: SubscriptionDataRepository
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +48,7 @@ class UserProfileActivity : AppCompatActivity() {
                 this,
                 UserProfileViewModelFactory(application, repo = repo)
             )[UserProfileViewModel::class.java]
+        auth = Firebase.auth
         setContent {
             BasicsCodelabTheme {
                 val state by viewModel.state.collectAsState(initial = UserProfileState())
@@ -128,16 +134,26 @@ class UserProfileActivity : AppCompatActivity() {
     private fun BasicUserProfile() {
         Log.wtf(TAG, "Loading Basic Profile")
         Surface {
-            Row(
+            Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column {
-                    Text(text = "This is your basic profile")
+                Text(text = "This is your basic profile", style = MaterialTheme.typography.body1)
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.End
+            ) {
+                Button(onClick = {
+                    logout()
+                }) {
+                    Text(text = "Log out")
                 }
             }
         }
+
     }
 
     @Preview(showBackground = true, widthDp = 320, heightDp = 320)
@@ -152,13 +168,22 @@ class UserProfileActivity : AppCompatActivity() {
     private fun PremiumUserProfile() {
         Log.wtf(TAG, "Loading Premium Profile")
         Surface {
-            Row(
+            Column(
                 modifier = Modifier.fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column {
-                    Text(text = "This is your premium profile")
+                Text(text = "This is your premium profile", style = MaterialTheme.typography.body1)
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.End
+            ) {
+                Button(onClick = {
+                    logout()
+                }) {
+                    Text(text = "Log out")
                 }
             }
         }
@@ -178,10 +203,14 @@ class UserProfileActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun logout() {
+        auth.signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
     companion object {
         private const val TAG: String = "UserProfileActivity"
     }
 
 }
-
-
